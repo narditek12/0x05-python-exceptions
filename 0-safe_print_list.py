49f8a927ace875f09498f81d7a3cf2b0143a3a2b
narditek12 <51337463+narditek12@users.noreply.github.com>
2022-06-21 18:15:06 +0300

    Update 0-safe_print_list.py

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,17 +1,12 @@
+
 #!/usr/bin/python3
-
-
 def safe_print_list(my_list=[], x=0):
-    idx = 0
-
+    cnt = 0
     try:
-        for i in my_list:
-            if idx < x:
-                print('{}'.format(my_list[idx]), end='')
-                idx += 1
-
-        print()
-    except TypeError:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end='')
+            cnt += 1
+    except IndexError:
         pass
-    finally:
-        return idx
+    print()
+    return (cnt)
